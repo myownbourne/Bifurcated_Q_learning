bc710d72b8bfb5a2c45e9735f35659d92249e96e
unknown <810060354@qq.com>
2026-04-24 23:05:30 +0800

    'add'

--- real_data/Table_S2S3.r
+++ real_data/Table_S2S3.r
@@ -1,4 +1,4 @@
-read_data = read.csv("r_add.csv")
+read_data = read.csv("r_add.csv") # and r.csv
 new_array = array(data = read_data$value, dim = c(max(read_data$row), max(read_data$col), max(read_data$matrix)))
 sim_result = new_array
 
